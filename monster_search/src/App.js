31517c6d4470b799f9841edd10309d35da4e51b7
ChadH28 <chadhoosain@gmail.com>
2022-02-02 11:49:18 +0200

    worked with map method on lists

--- monster_search/src/App.js
+++ monster_search/src/App.js
@@ -1,56 +1,68 @@
-import logo from './logo.svg';
+
 import './App.css';
-import React, { Component } from 'react';
+import React from 'react';
 
 // Function based
-  // function App() {
-  //   return (
-  //     <div className="App">
-  //       <header className="App-header">
-  //         <img src={logo} className="App-logo" alt="logo" />
-  //         <p>
-  //           yo
-  //         </p>
-  //         <a
-  //           className="App-link"
-  //           href="https://reactjs.org"
-  //           target="_blank"
-  //           rel="noopener noreferrer"
-  //         >
-  //           Learn React
-  //         </a>
-  //       </header>
-  //     </div>
-  //   );
-  // }
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           yo
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
 
 
 // Class based
-class App extends Component {
+class App extends React.Component {
   constructor() {
     super();
-    // calling state property
     this.state = {
-      string: 'whats happening'
+      // calling state property
+      // string: 'whats happening'
+      coaches: [
+        {
+          id: '1',
+          name: 'Xavi'
+        },
+        {
+          id: '2',
+          name: 'Lampard'
+        },
+        {
+          id: '3',
+          name: 'Gerrard'
+        },
+        {
+          id: '4',
+          name: 'Pep'
+        },
+        {
+          id: '5',
+          name: 'Tuchel'
+        },
+      ]
     }
   }
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          {/* curly brackets for javascript expression for variables */}
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            {this.state.string}
-          </p>
-          <button
-            className="App-link"
-            // changes the state of the property string
-            onClick={() => this.setState({string: 'you have a click'})}
-          >
-            Learn React
-          </button>
-        </header>
+        {
+          // this.state.property
+          this.state.coaches.map(coach => <h1 key={coach.id}> {coach.name} </h1>)
+        }
       </div>
     );
   }
