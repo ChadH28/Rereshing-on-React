5d0f8cd2e3cd83fd9fc8809f97068c9f5994125c
ChadH28 <chadhoosain@gmail.com>
2022-02-08 14:24:47 +0200

    worked on searchbar component state

---
+++ monster_search/src/components/searchbar/searchBar.js
@@ -0,0 +1,12 @@
+// Single card component
+import './searchBar.css';
+import React from 'react';
+
+// Function based
+export const Searchbar = (props) => {
+  return (
+    <div className="searchbar">
+      {/* <input className="searchbar-input" type='search' placeholder='Search Bots here..'/> */}
+    </div>
+  );
+}
