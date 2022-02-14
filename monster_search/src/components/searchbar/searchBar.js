e0fd706c060674b34f0decbdb2bdfda03e039107
ChadH28 <chadhoosain@gmail.com>
2022-02-14 08:25:36 +0200

    worked on searchbar state and onchange function

--- monster_search/src/components/searchbar/searchBar.js
+++ monster_search/src/components/searchbar/searchBar.js
@@ -3,10 +3,19 @@ import './searchBar.css';
 import React from 'react';
 
 // Function based
-export const Searchbar = (props) => {
+export const Searchbar = ({placeholder,handleChange}) => {
   return (
     <div className="searchbar">
-      {/* <input className="searchbar-input" type='search' placeholder='Search Bots here..'/> */}
+      {/* changing the state value of searchfield : '' to whatever the value the user tye in then running a callback funtion to update the values state */}
+      <input
+        className="searchbar-input"
+        type='search'
+        placeholder={placeholder}
+        // updating state evertime something is typed
+        // onChange={e => this.setState({ searchField: e.target.value }, () => console.log(this.state))}
+        // placeholder='Search Bots here..'
+        onChange={handleChange}
+      />
     </div>
   );
 }
